386e162794f15935824815d3c2061d0fb842d182
Juri Lucci <juri.lucci@sinnovasistemi.it>
2021-11-12 17:07:20 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {CardComponent} from './card/card.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RestartDialogComponent} from './restart-dialog/restart-dialog.component';
-import {MatIconModule} from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CardComponent,
-    RestartDialogComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatIconModule,
-    BrowserAnimationsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
